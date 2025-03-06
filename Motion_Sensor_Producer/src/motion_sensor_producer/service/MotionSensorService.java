c4da28ad64d893d480b420125d791747ce533c3e
Chamika Dilshan <chamikadilshan1123@gmail.com>
2025-03-06 17:02:17 +0530

    rm -rf

--- Motion_Sensor_Producer/src/motion_sensor_producer/service/MotionSensorService.java
+++
@@ -1,9 +0,0 @@
-package motion_sensor_producer.service;
-
-import motion_sensor_producer.command.MotionSensorCommand;
-
-public interface MotionSensorService {
-    void publishMotionSensorEvent(String event);
-    void startDetection();
-    void processMotionCommand(MotionSensorCommand motionCommand);
-}
--- Smart_Light_Controller_Subscriber/src/smart_light_subscriber/service/SmartLightService.java
+++
@@ -1,6 +0,0 @@
-package smart_light_subscriber.service;
-
-public interface SmartLightService {
-    void turnOn();
-    void turnOff();
-}
